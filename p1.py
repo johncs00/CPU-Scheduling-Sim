b055b0d24b32f7d9f89c995a7c8c41a5465ab537
johncs00 <54725234+johncs00@users.noreply.github.com>
2020-07-15 14:45:31 -0400

    Add files via upload

--- p1.py
+++ p1.py
@@ -18,12 +18,29 @@ class rand:
 
 
 class Process:
-    def __init__(self, name, cpu_time, io_time, state):
+    def __init__(self, name, cpu_time, io_time, state, turn_time, wait_time):
         self.n = name
         self.cpu = cpu_time
         self.io = io_time
         self.s = state
-        #needs setters
+        self.turn = turn_time
+        self.wait = wait_time
+    def getName():
+        return self.n
+    def setName(name):
+        self.n = name
+    def getCpu():
+        return self.cpu
+    def setCpu(cpu_time):
+        self.cpu = cpu_time
+    def getIo():
+        return self.io
+    def setIo(io_time):
+        self.io = io_time
+    def getState():
+        return self.s
+    def setState(state):
+        self.s = state
 
 
 
@@ -57,4 +74,3 @@ for pro in range(int(numpros)):
 
 
 print(sys.argv[2])
-
