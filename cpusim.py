7521b6692ab51a64c5aeeb01d2246fd83a9b5d6f
macksjeremy <31860185+macksjeremy@users.noreply.github.com>
2020-07-14 21:17:43 -0400

    Add files via upload

---
+++ cpusim.py
@@ -0,0 +1,39 @@
+import sys
+import time
+import random as rand
+import math
+
+print("Testing")
+r = rand.random()
+print(r)
+
+
+min = 0
+max = 0
+sum = 0
+#replace l with argv[3]
+#replace upperbound with argv[4]
+iterations = 10000000
+l = 0.001;
+upperbound = 3000
+
+for i in range(iterations):
+    #replace random() with drand48
+    r = rand.random() #/ * uniform # dist[0.00, 1.00) -- also check out random() * /
+    x = -math.log(r) / l# / lambda; / * log() is natural log * /
+    # / * avoid values that are far down the "long tail" of the distribution * /
+    if (x > upperbound):
+        i -= 1
+        continue
+    if (i < 20):
+        print("x is ", x)
+    sum += x
+    if (i == 0 or x < min):
+        min = x
+    if ( i == 0 or x > max ):
+        max = x
+
+    avg = sum / iterations
+    print( "minimum value: ", min)
+    print( "maximum value: ", max)
+    print( "average value: ", avg)
