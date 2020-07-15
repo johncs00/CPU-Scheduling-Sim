1a2ac0185226c27c43a36e392c5ce8d0b83d0e9b
macksjeremy <31860185+macksjeremy@users.noreply.github.com>
2020-07-14 22:44:02 -0400

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,60 @@
+import sys
+import time
+import random as rand
+import math
+
+class rand:
+	def __init__(self,seed):
+		self.n = seed
+	def setSeed(self, seed):
+		self.n = seed
+	def lcg(self):
+		self.n = (25214903917 * self.n + 11) & (2**48 - 1)
+		return self.n
+	def srand48(self, seed):
+		self.n = (seed << 16) + 0x330e
+	def drand48(self):
+		return self.lcg() / 2**48
+
+
+class Process:
+    def __init__(self, name, cpu_time, io_time, state):
+        self.n = name
+        self.cpu = cpu_time
+        self.io = io_time
+        self.s = state
+        #needs setters
+
+
+
+inputlen = len(sys.argv)
+if (not(inputlen == 9 or inputlen == 8)):
+    #make the code fucking stop here
+    print("bad args")
+numpros = sys.argv[1]
+seed = sys.argv[2]
+l = sys.argv[3]
+expceil = sys.argv[4]
+switchtime = sys.argv[5]
+alpha = sys.argv[6]
+tslice = sys.argv[7]
+rradd = "END"  # end
+#check if the optional one is added
+if (inputlen == 9):
+    print(sys.argv[8])
+    if (sys.argv[8] == "END"):
+        rradd = "END"
+    elif (sys.argv[8] == "BEGINNING"):
+        rradd = "Beginning"
+    else:
+        print("invalid arg 8")
+s = 'A'
+##Run through the process, creating one for each s
+for pro in range(int(numpros)):
+    print(s)
+    #process(s, )
+    s = chr(ord(s) + 1)
+
+
+print(sys.argv[2])
+
